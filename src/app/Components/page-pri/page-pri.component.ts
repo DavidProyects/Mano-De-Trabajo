9cb2c32d097d3e982a54872ae5f10f70c1b3bde1
Julian Hernandez <95661957+DavidProyects@users.noreply.github.com>
2022-03-24 15:03:19 -0500

    master

--- src/app/Components/page-pri/page-pri.component.ts
+++ src/app/Components/page-pri/page-pri.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Resgister } from './../../models/register';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-page-pri',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagePriComponent implements OnInit {
 
-  constructor() { }
+  productForm: FormGroup;
+
+  constructor(private fb: FormBuilder,) { 
+    
+    this.productForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      Correo: ['', Validators.required],
+      telefono: ['', Validators.required],
+      contraseña: ['', Validators.required]
+    })
+  }
+ 
 
   ngOnInit(): void {
+  }
+
+  Registrar(){
+    console.log(this.productForm)
+
+    const REGISTER: Resgister = {
+
+      nombre: this.productForm.get('nombre')?.value,
+      apellido: this.productForm.get('apellido')?.value,
+      Correo: this.productForm.get('Correo')?.value,
+      telefono: this.productForm.get('telefono')?.value,
+      contraseña: this.productForm.get('contraseña')?.value
+
+
+    }
+    console.log(REGISTER);
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Registro completo, Revisa tu buzon de Correo',
+      showConfirmButton: false,
+      timer: 2000
+    })
+    
   }
 
 }
