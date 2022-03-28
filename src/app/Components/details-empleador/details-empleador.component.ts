c50d4d41fdec3f0fac0ce457050f42dd3f94e816
DavidProyects <judahernandez20@gmail.com>
2022-03-27 23:47:20 -0500

    master

--- src/app/Components/details-empleador/details-empleador.component.ts
+++ src/app/Components/details-empleador/details-empleador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-details-empleador',
@@ -11,5 +12,16 @@ export class DetailsEmpleadorComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  metodosdepago(){
+    Swal.fire({
+      title: 'Escoge tu metodo de pago',
+      text: 'Seras redirigido a una pagina extarna para completar tu pago',
+      imageUrl: '../assets/metodos.jpg',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Custom image',
+    })
+  }  
 
 }
